88db7fd1854af5123de4b9f2ce57463a3d90d1a9
Gabor Soos <soos.gabor86@gmail.com>
2023-01-07 14:57:24 +0100

    add index for shortid for faster access

--- src/models/url.model.ts
+++ src/models/url.model.ts
@@ -4,6 +4,6 @@ export const UrlModel = mongoose.model(
   'Url',
   new Schema({
     originalUrl: { type: String, required: true },
-    shortId: { type: String, required: true },
+    shortId: { type: String, required: true, index: { unique: true } },
   }),
 );
